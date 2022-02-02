65b71d4ac8ce23d10f5c7c977a27bf2e9652ccc0
enncy <877526278@qq.com>
2022-02-02 21:59:53 +0800

    docs(core/walker.ts): add docs in walker

--- src/core/walker.ts
+++ src/core/walker.ts
@@ -1,5 +1,9 @@
 import EventEmitter from "events";
 
+/**
+ * all the event trigger name of Walker,
+ * and the type is the first argument of event handler
+ */
 export interface WalkerEvents<T> {
     walk: T;
     peek: T;
@@ -7,18 +11,56 @@ export interface WalkerEvents<T> {
     add: [number, T[]];
     append: [number, T[]];
     remove: [number, number];
-
     update: number;
-
     back: number;
     jump: number;
     rewalk: number;
-
     start: number;
     stop: number;
- 
 }
 
+/**
+ * The Walker class provides an iterator-like function
+ *
+ * that helps you run, stop, jump, back, rerun, etc. the list of data
+ *
+ * @extends EventEmitter
+ * @example
+ * ```ts
+ * interface Script{
+ *      name: string
+ * }
+ * const scripts:Script[]  = [{name:'goto'},{name:'click'},{name:'close'}]
+ * // 1. you can pass the data list to the constructor
+ * const walker = new Walker<Script>(scripts)
+ * // 2. or use function `add(...data)`
+ * // walker.add(scripts)
+ *
+ *
+ * walker.step      // 0
+ * walker.walk()    // {name:'goto'}
+ * walker.step      // 1
+ *
+ * walker.end()     // false
+ * walker.walk()    // {name:'click'}
+ * walker.walk()    // {name:'close'}
+ * walker.end()     // true
+ * walker.step      // 3
+ *
+ *
+ *
+ * walker.reWalk()  // reset the step of walker
+ * walker.step      // 0
+ *
+ * // so if you just want to walk all the step :
+ *
+ * while(!walker.end()){
+ *      const script = walker.walk()
+ *      // ...do something
+ * }
+ *
+ * ```
+ */
 export class Walker<T> extends EventEmitter {
     /** walk step index */
     protected _step: number = 0;
@@ -30,16 +72,18 @@ export class Walker<T> extends EventEmitter {
         super();
         this.list = array || [];
     }
-
     get step() {
         return this._step;
     }
-
     private set step(s) {
         this._step = s;
     }
 
-    /** walk , and return the item value  */
+    /**
+     * fetch data in list, also let the step plus 1.
+     *
+     * if your data is promise function, use `await` to wait.
+     */
     walk(): T | undefined | Promise<T | undefined> {
         if (this._isStop) {
             return new Promise<T>((resolve, reject) => {
@@ -56,7 +100,9 @@ export class Walker<T> extends EventEmitter {
         }
     }
 
-    /** walk the previous step */
+    /**
+     * let step minus 1
+     */
     back() {
         if (!this._isStop) {
             this.emit("back", this.step);
@@ -78,6 +124,9 @@ export class Walker<T> extends EventEmitter {
         return this;
     }
 
+    /**
+     * make the step 0
+     */
     reWalk() {
         if (!this._isStop) {
             this.emit("rewalk", this.step);
@@ -96,7 +145,11 @@ export class Walker<T> extends EventEmitter {
         return this;
     }
 
-    /** stop walking */
+    /**
+     * stop walking
+     *
+     * let all walk function waiting until method `walker.start()` is called
+     */
     stop() {
         this._isStop = true;
         this.emit("stop", this.step);
@@ -114,18 +167,24 @@ export class Walker<T> extends EventEmitter {
         return this.step === this.list.length;
     }
 
+    /**
+     * get the data of a step, but do not update the step
+     */
     peek(step?: number) {
         const item = this.list.at(step || this.step);
         this.emit("peek", item);
         return item;
     }
 
+    /**
+     * get all the data of a step, but do not update the step
+     */
     peekAll() {
         this.emit("peekall", this.list);
         return this.list;
     }
 
-    /** add actions at tail */
+    /** add data at tail */
     append(...actions: T[]) {
         this.emit("append", [this.list.length, actions]);
         this.list.splice(this.list.length, 0, ...actions);
@@ -133,7 +192,7 @@ export class Walker<T> extends EventEmitter {
         return this;
     }
 
-    /** add actions at next */
+    /** add data at next */
     add(...actions: T[]) {
         this.emit("add", [this.step, actions]);
         this.list.splice(this.step, 0, ...actions);
@@ -141,7 +200,7 @@ export class Walker<T> extends EventEmitter {
         return this;
     }
 
-    /** add actions at some step */
+    /** add data at some step */
     addAt(step: number, ...actions: T[]) {
         this.emit("add", [step, actions]);
         this.list.splice(step, 0, ...actions);
@@ -149,7 +208,7 @@ export class Walker<T> extends EventEmitter {
         return this;
     }
 
-    /** remove current action and return */
+    /** remove current data and return */
     remove(count?: number) {
         this.emit("remove", [this.step, count || 1]);
         this.list.splice(this.step, count || 1);
@@ -157,7 +216,7 @@ export class Walker<T> extends EventEmitter {
         return this;
     }
 
-    /** remove action at some step and return */
+    /** remove data at some step and return */
     removeAt(index: number, count?: number) {
         this.emit("remove", [index, count]);
         this.list.splice(index, count);
